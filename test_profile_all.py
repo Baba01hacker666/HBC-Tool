46dcc563154a79e179c45b810266aa2044a66c71
Baba01hacker666 <117832562+Baba01hacker666@users.noreply.github.com>
2026-04-05 12:09:20 +0000

    Fix pytest fixture error by removing test_profile_all.py

--- test_profile_all.py
+++
@@ -1,9 +0,0 @@
-import time
-import subprocess
-
-def test_cmd(cmd):
-    start = time.time()
-    subprocess.run(cmd, shell=True, stdout=subprocess.DEVNULL)
-    return time.time() - start
-
-print(test_cmd("python3 hbctool/test.py"))
